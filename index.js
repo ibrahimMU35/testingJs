0b805de28ffb938d8c07f901ec7b000d1e6b6265
Ibrahim MUSTAFA <ibrahimmu3533@gmail.com>
2025-10-31 05:44:21 +0100

    update

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log(`hello`);
+// console.log(` i like pizza`);
+// window.alert(`this is an alert!`);
+// window.alert(`i like pizza!`);
+
+// this is a comment
+
+/*
+this 
+is 
+a 
+comment 
+*/
